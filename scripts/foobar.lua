cfb271b2cc6117722104246c7f35fca6f2dd58d9
Xing Xing <mikespook@gmail.com>
2016-12-12 11:03:48 +1300

    fixed print format

--- scripts/foobar.lua
+++ scripts/foobar.lua
@@ -1,4 +1,4 @@
-ghoko.Debugf("%s %V", ghoko.Id, ghoko.Params)
+ghoko.Debugf("%s %+v", ghoko.Id, ghoko.Params)
 if ghoko.Params["test"] == nil then
 	ghoko.Params["test"] = "abc"
 	ghoko.Call(ghoko.Id, "foobar", ghoko.Params)
